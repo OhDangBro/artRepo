57283a7167459dbf5206f36f0b788ec71021e2de
William Gehrke <williamegehrke@gmail.com>
2023-01-31 13:43:18 -0500

    set up hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,17 +6,16 @@ import HeroFern from "../../assets/img/fernBorder.svg"
 const Hero = () => {
   return (
     <div className="hero">
-      <img className="topImageHero" src={HeroFern}/>
-      <div className="heroTextContainer">
+    <div className="heroTextContainer">
       <div className="hero-header">
-        <h1 className="heroCursive">Header Text</h1>
+        <h1 className="heroCursive">Deborah Stair</h1>
+        <p className="heroSecondText">Artistic Inovations</p>
+        <p className="heroThirdText">Specializing in creating stunning faux finishes, murals, window art, and custom illustrations that are both visually appealing and tailored to your needs.</p>
       </div>
-      <div className="hero-text">
-        <p>Some words under the header</p>
-      </div>
-      </div>
-      <img className="bottomImageHero" src={HeroFern}/>
     </div>
+    <img className="topImageHero" src={HeroFern} />
+    <img className="bottomImageHero" src={HeroFern} />
+  </div>
   );
 };
 
